c02b065d911b921a1809fe2cd013e92cae1bdae6
God Orng <neuporng123@gmail.com>
2025-11-17 11:38:15 +0700

    add api description

--- src/modules/feature-flag/models/feature-flag/request.ts
+++ src/modules/feature-flag/models/feature-flag/request.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateFeatureFlagRequest {
+    @ApiProperty({ description: 'Name of the feature flag', example: 'New Dashboard' })
     name: string;
+    @ApiProperty({ description: 'Description of the feature flag', example: 'Enables the new dashboard for users.', required: false })
     description?: string;
+    @ApiProperty({ description: 'Key of the feature flag', example: 'new_dashboard' })
     key: string;
+    @ApiProperty({ description: 'Indicates whether the feature flag is enabled', example: true })
     enabled: boolean;
 }
